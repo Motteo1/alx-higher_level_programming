659a4710c0c6609a012e4520d48b7f04c372db0f
Motteo1 <timothykirungu@gmail.com>
2022-08-31 12:31:43 -0700

    Init

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,4 +7,11 @@ def roman_to_int(roman_string):
     num = 0
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i, j in zip(roman_string, roman_string[1:]):
-
+        if i not in dic.keys():
+            return 0
+        elif dic[i] >= dic[j]:
+            num += dic[i]
+        else:
+            num -= dic[i]
+    num += dic[roman_string[-1]]
+    return num

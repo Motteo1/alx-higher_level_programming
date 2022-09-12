5defe4ad7a9816912831af677082a540b6233644
Motteo1 <timothykirungu@gmail.com>
2022-09-12 02:52:29 -0700

    Pycodestyle no like whitespaces

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,6 @@ def safe_print_division(a, b):
         div = a / b
     except (ZeroDivisionError, TypeError):
         div = None
-    finally: 
-        print("Inside result: {}",format(div))
+    finally:
+        print("Inside result: {}".format(div))
     return div

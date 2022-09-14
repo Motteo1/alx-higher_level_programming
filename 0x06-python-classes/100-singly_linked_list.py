8fc8aadb9113f03dc0834a73ac7c08a1007f28a9
Motteo1 <timothykirungu@gmail.com>
2022-09-14 07:47:49 -0700

    Insert sorted

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -103,3 +103,35 @@ class SinglyLinkedList:
         """
         String representation of singly linked list needed to print
         """
+        string = ""
+        tmp = self.__head
+        while tmp is not None:
+            string += str(tmp.data)
+            tmp = tmp.next_node
+            if tmp is not None:
+                string += "\n"
+        return string
+
+    def sorted_insert(self, value):
+        """
+        Inserts new nodes into singly linked list in sorted order
+
+        Args:
+        value: int data for node
+        """
+        new = Node(value)
+        if self.__head is None:
+            self.__head = new
+            return
+
+        tmp = self.__head
+        if new.data < tmp.data:
+            new.next_node = self.__head
+            self.__head = new
+            return
+
+        while (tmp.next_node is not None) and (new.data > tmp.next_node.data):
+            tmp = tmp.next_node
+        new.next_node = tmp.next_node
+        tmp.next_node = new
+        return

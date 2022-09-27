0e5c6ce6158125e07eb4d7ed4b2f12592fd12252
Motteo1 <timothykirungu@gmail.com>
2022-09-27 11:39:31 -0700

    fix pycodestyle

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 8-class_to_json
+
+Contains function that
+returns dictionary description with simple data structure
+(list, dictionary, string)
+for JSON serialization of an object
+"""
+
+
+def class_tojson(obj):
+    """Returns decrionary description with simple data structure
+       (list, dictionary, string)
+       for JSON serialization of an object
+    Args:
+        obj: python object
+    """
+    return obj.__dict__

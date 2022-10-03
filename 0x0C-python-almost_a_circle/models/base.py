a08dda5e79ba84e5aa8518491dc61522f02825cf
Motteo1 <timothykirungu@gmail.com>
2022-10-03 08:39:57 -0700

    Fic class methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Module contains class Base
+
 Contains private class __nb_objects, and class constructor __init__
 Returns JSON string representation of list dictionaries
 Saves JSON strings of instance dictionaries into file
@@ -40,4 +41,43 @@ class Base():
             self.id = Base.__nb_objects
 
     @staticmethod
-    def to_json_string
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of list dict"""
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns Python obj of JSON string representation"""
+        if json_string is None or len(json_string) == 0:
+            json_string = "[]"
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save json strings of all instances into file"""
+        objs = []
+        if list_objs is not None:
+            for o in list_objs:
+                objs.append(cls.to_dictionary(o))
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(objs))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns instance with attributes already set"""
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns list of instances"""
+        filenamee = cls.__name__ + ".json"
+        l = []
+        try:

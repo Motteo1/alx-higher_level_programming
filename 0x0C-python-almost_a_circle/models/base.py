41f1c084e924bf204ee7961312b69279273df77f
Motteo1 <timothykirungu@gmail.com>
2022-10-03 15:55:13 -0700

    pycode errors

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -79,15 +79,15 @@ class Base():
     def load_from_file(cls):
         """Returns list of instances"""
         filenamee = cls.__name__ + ".json"
-        a = []
+        l = []
         try:
             with open(filenamee, "r") as f:
                 instances = cls.from_json_string(f.read())
             for i, dic in enumerate(instances):
-                a.append(cls.create(**instances[i]))
+                l.append(cls.create(**instances[i]))
         except pass:
             pass
-        return a
+        return l
 
     @classmethod
     def save_to_file_csv(cls, list_objs):

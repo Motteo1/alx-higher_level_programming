034329d1871ec8233b79f6176eaa14fe2941b6bc
Motteo1 <timothykirungu@gmail.com>
2022-11-02 15:13:18 -0700

    Update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,18 +1,19 @@
--- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows_rate.sql
 -- run this first to import a SQL dump -->
---      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
---      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
+--      echo "CREATE DATABASE hbtn_0d_tvshows_rate;" | mysql -uroot -p
+--      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows_rate
 --
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
--- each record should display: tv_genres.name - number_shows
--- don’t display a genre that doesn’t have any shows linked
--- results must be sorted in descending order by the number of shows linked
+-- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
+-- each record should display: tv_genres.name - rating sum
+-- results must be sorted in descending order by the rating
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 
-    SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
+    SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
       FROM tv_genres
 INNER JOIN tv_show_genres
-        ON tv_genres.id = tv_show_genres.genre_id
-  GROUP BY genre
-  ORDER BY number_shows DESC;
+        ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings
+        ON tv_show_ratings.show_id = tv_show_genres.show_id
+  GROUP BY name
+  ORDER BY rating DESC;

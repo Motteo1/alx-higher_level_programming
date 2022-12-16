3fe61cd7ff2bf9f1148d7a839e2bea1c12ac21e4
Motteo1 <timothykirungu@gmail.com>
2022-12-16 14:17:53 +0000

    Make executable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ fetch https://intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopn('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html - response.read()
         print('body response:')
         print("\t- type: {}".format(type(html)))

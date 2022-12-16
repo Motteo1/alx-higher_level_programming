4d8521fe983e936458ce7ac3691fc57e64de24d3
Motteo1 <timothykirungu@gmail.com>
2022-12-16 14:14:52 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,3 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopn('https://intranet.hbtn.io/status') as response:
         html - response.read()
         print('body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(type))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))

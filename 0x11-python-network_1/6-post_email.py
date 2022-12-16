a2025f0dc4326d330d2aabbc639975849122edd9
Motteo1 <timothykirungu@gmail.com>
2022-12-16 17:04:16 +0000

    Update task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-        url = argv[1]
-            payload = {'email': argv[2]}
-                r = requests.post(url, data=payload)
-                    print(r.text)
+    url = argv[1]
+    payload = {'email': argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)

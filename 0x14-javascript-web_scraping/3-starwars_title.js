0be636b556693c2d3f6583b710e86f58f34f6ba8
Motteo1 <timothykirungu@gmail.com>
2023-01-17 19:45:05 +0000

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(response.JSON.title);
+  } else {
+    console.log('Error Code:' + response.statusCode);
+  }
+});
